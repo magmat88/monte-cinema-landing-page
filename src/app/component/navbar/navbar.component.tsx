372b7317e8a2edaa511c5addffa6b0e963b989f7
magmat88 <magdalena.matusiak88@gmail.com>
2022-01-28 23:13:32 +0100

    Added logo and buttons

--- src/app/component/navbar/navbar.component.tsx
+++ src/app/component/navbar/navbar.component.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import logo from '../../images/logo.svg';
 import './navbar.component.scss';
 
 export function NavBar(): JSX.Element {
     return (
-        <div className="nav-bar">
-            <p>NavBar component</p>
-        </div>
+        <nav className="nav-bar">
+            <figure>
+                <img src={logo} alt="monte cinema logo" />
+            </figure>
+            <div className="btns-container">
+                <button onClick={() => console.log('register on click')}>
+                    Register
+                </button>
+                <button onClick={() => console.log('login on click')}>
+                    Login
+                </button>
+            </div>
+        </nav>
     );
 }
