47a6f1fe65baa0a791be0bb1435c033ada37008d
magmat88 <magdalena.matusiak88@gmail.com>
2022-02-01 01:40:07 +0100

    in progress

--- src/app/component/onlineBooking/onlineBooking.component.tsx
+++ src/app/component/onlineBooking/onlineBooking.component.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { monteCinemaLogo } from '../navbar/monteCinemaLogo';
 import './onlineBooking.component.scss';
 
 export function OnlineBooking(): JSX.Element {
     return (
-        <div className="online-booking">
-            <p>OnlineBooking component</p>
-        </div>
+        <section className="online-booking">
+            <h1>Online booking</h1>
+            <p>Soon</p>
+
+            <article className="notification">
+                <figure className="logo">{monteCinemaLogo}</figure>
+                <p>
+                    Leave us your email address and we'll let you know as soon
+                    as the machine will start.
+                </p>
+                <form>
+                    <input />
+                    <button className="btn btn-contrast"></button>
+                </form>
+            </article>
+        </section>
     );
 }
