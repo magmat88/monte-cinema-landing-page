ccd44beada15105aa6ca94ebfa955d542acdaf40
magmat88 <magdalena.matusiak88@gmail.com>
2022-02-01 13:02:52 +0100

    Created online booking component with responsive view

--- src/app/component/onlineBooking/onlineBooking.component.tsx
+++ src/app/component/onlineBooking/onlineBooking.component.tsx
@@ -14,9 +14,10 @@ export function OnlineBooking(): JSX.Element {
                     Leave us your email address and we'll let you know as soon
                     as the machine will start.
                 </p>
-                <form>
-                    <input />
-                    <button className="btn btn-contrast"></button>
+                <form className="notification-form">
+                    <label htmlFor="email" className="notification-label">Email</label>
+                    <input id="email"className="inputField" placeholder="e.g. mynameislaura@mail.com" />
+                    <button className="btn btn-contrast">Let me know!</button>
                 </form>
             </article>
         </section>
